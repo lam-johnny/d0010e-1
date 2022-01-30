efbc110a1888ee4feb49a7ae8744ccc00dac666c
add33m <48681209+add33m@users.noreply.github.com>
2022-01-30 17:08:20 +0100

    Added base assignment code for labb2

---
+++ labb2/src/Driver.java
@@ -0,0 +1,11 @@
+import level.Level;
+import level.LevelGUI;
+import level.Room;
+
+public class Driver {
+
+	public void run() {
+		
+	}
+
+}
---
+++ labb2/src/level/Room.java
@@ -0,0 +1,24 @@
+package level;
+
+import java.awt.Color;
+
+
+public class Room { 
+	
+	public Room(int dx, int dy, Color color) {
+		
+	}
+
+	public void connectNorthTo(Room r) {
+		
+	}
+	public void connectEastTo(Room r) {
+		
+	}
+	public void connectSouthTo(Room r) {
+		
+	}
+	public void connectWestTo(Room r) {
+		
+	}
+}

17695aa90dcb11aeea8a9e0b523f9abd148a9065
add33m <48681209+add33m@users.noreply.github.com>
2022-02-03 17:07:45 +0100

    Continued work on labb2

--- labb2/src/lab2/Driver.java
+++ labb2/src/lab2/Driver.java
@@ -1,4 +1,7 @@
 package lab2;
+import java.awt.Color;
+import java.util.Random;
+
 import lab2.level.Level;
 import lab2.level.LevelGUI;
 import lab2.level.Room;
@@ -6,7 +9,20 @@ import lab2.level.Room;
 public class Driver {
 
 	public void run() {
-		System.out.println("This is a print-out from the driver.");
+		Random r = new Random();
+		int numRooms = r.nextInt(5) + 3; // Random number of rooms between 3 and 8
+		Room[] rooms = new Room[numRooms];
+
+		for (int i = 0; i < numRooms; i++) {
+			rooms[i] = new Room(r.nextInt(10) + 1, r.nextInt(10) + 1,
+					new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+
+			if (i != 0) {
+				// rooms[i] är ditt current rum
+				rooms[i-1].connectNorthTo(rooms[i]);
+				rooms[i].connectSouthTo(rooms[i-1]); //Detta gör att vi får en dubbelriktad graf
+			}
+		}
 	}
 
 }
--- labb2/src/lab2/level/Room.java
+++ labb2/src/lab2/level/Room.java
@@ -4,26 +4,34 @@ import java.awt.Color;
 
 
 public class Room {
-	private Room northDoor = null;
-	private Room eastDoor = null;
-	private Room southDoor = null;
-	private Room westDoor = null;
-	private Color floorColor = null;
+	Color floorColor = null;
+	int width;
+	int height;
+	int x;
+	int y;
+
+	Room northDoor = null;
+	Room eastDoor = null;
+	Room southDoor = null;
+	Room westDoor = null;
 	
 	public Room(int dx, int dy, Color color) {
-		
+		this.floorColor = color;
+		this.width = dx;
+		this.height = dy;
+		System.out.println(dx + " " + dy + " " + color);
 	}
 
 	public void connectNorthTo(Room r) {
-		
+		this.northDoor = r;
 	}
 	public void connectEastTo(Room r) {
-		
+		this.eastDoor = r;
 	}
 	public void connectSouthTo(Room r) {
-		
+		this.southDoor = r;
 	}
 	public void connectWestTo(Room r) {
-		
+		this.westDoor = r;
 	}
 }
